c8a24978196da1dcefbdb84c60528efaf8c3045a
Haileab <womahaileab1428@gmail.com>
2026-02-25 20:31:42 +0300

    my first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require("node:path");
+
+
+
+const express = require("express");
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => {
+  res.render("index", { message: "EJS rocks!" });
+});
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  // This is important!
+  // Without this, any startup errors will silently fail
+  // instead of giving you a helpful error message.
+  if (error) {
+    throw error;
+  }
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
