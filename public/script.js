49911ff782a5696c6dad3535ab93d13bb6beb849
Haileab <womahaileab1428@gmail.com>
2026-03-03 17:27:23 +0300

    working on the logic off the form

--- public/script.js
+++ public/script.js
@@ -1,8 +1,55 @@
 const dialog = document.getElementById('myDialog');
 const showButton = document.getElementById('showDialogButton');
+const myform = document.getElementById('myform')
+const Name = document.getElementById('Name')
+const message = document.getElementById('Message')
+const nameError = document.getElementById('name')
+const messageError = document.getElementById('message')
+const cancelButton = document.getElementById('cancelButton')
+const regex = /^[a-zA-Z]+$/;
+const messageRegex = /./s;
 
 // Show the modal dialog
 showButton.addEventListener('click', () => {
     dialog.showModal()
     dialog.style.display = 'block'
 });
+
+
+cancelButton.addEventListener('click', (e) => {
+    e.preventDefault();           // stops form submission
+    dialog.style.display = 'none'
+});
+
+// Validate The Form
+
+function validateForm(event){
+     event.preventDefault();
+    if(!regex.test(Name.value) || !(Name.value)){
+       nameError.style.display = "block"
+       nameError.textContent = "Please type alphabbetic letters only"
+        nameError.style.color = '#EA0063'
+        nameError.style.fontStyle = "italic"
+     
+    }
+    else if(!message.value.trim() || !messageRegex.test(message.value)){
+            nameError.style.display = "none";
+            messageError.style.display = "block"
+            messageError.textContent = "Please type alphabbetic letters only"
+            messageError.style.color = '#EA0063'
+            messageError.style.fontStyle = "italic"
+    }
+    else {
+        messageError.style.display = "none"
+          alert("Your Message Have been added succesfully")
+          Name.value = "";
+          message.value = "";
+       dialog.close()
+        dialog.style.display = 'none'
+
+    }
+}
+
+myform.addEventListener('submit', validateForm)
+//Close the modal dialog
+
